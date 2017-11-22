dcdf13834b3008aa00ab1ec80752019e4c299496
Sifanele <sg.goqo@gmail.com>
2017-11-23 00:48:09 +0200

    Create and implement recipe contract.

--- Recipe.Service/IRecipeService.cs
+++ Recipe.Service/IRecipeService.cs
@@ -1,11 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using Recipe.Model;
 using System.Threading.Tasks;
 
 namespace Recipe.Service
 {
-    public class IRecipeService
+    public interface IRecipeService
     {
+        Task<Model.Recipe> GetById(string id);
+        Task<Model.Recipe> Create(Model.Recipe recipe);
+        void Update(Model.Recipe recipe);
+        void Delete(string id);
     }
 }
--- Recipe.Service/RecipeService.cs
+++ Recipe.Service/RecipeService.cs
@@ -2,10 +2,30 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Recipe.Model;
 
 namespace Recipe.Service
 {
-    public class RecipeService
+    public class RecipeService : IRecipeService
     {
+        public Task<Model.Recipe> Create(Model.Recipe recipe)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Model.Recipe> GetById(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Model.Recipe recipe)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
